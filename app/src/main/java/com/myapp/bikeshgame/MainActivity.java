4543e3e889ae73ae82593bf11cbb267cb97cb62e
suwalbikesh <suwalbikezh@gmail.com>
2019-09-25 17:10:54 +0545

    main code for the game.

--- app/src/main/java/com/myapp/bikeshgame/MainActivity.java
+++ app/src/main/java/com/myapp/bikeshgame/MainActivity.java
@@ -9,11 +9,11 @@ import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnfirst, btnsecond;
     private TextView txtPoints;
 
-    int randomNumber, randomNumber2;
+    int randomNumber, randomNumber2, value1, value2, points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +35,33 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         btnfirst.setText(String.valueOf(randomNumber));
         btnsecond.setText(String.valueOf(randomNumber2));
 
+        points = 0;
+
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btnfirst){
-            
+        if (v.getId() == R.id.btnfirst) {
+            value1 = Integer.parseInt(btnfirst.getText().toString());
+            value2 = Integer.parseInt(btnsecond.getText().toString());
+            if (value1 > value2) {
+                points = points + 1;
+            } else {
+                points = points - 1;
+            }
 
-        }
-        else if (v.getId() == R.id.btnsecond){
+            txtPoints.setText(String.valueOf(points));
+
+        } else if (v.getId() == R.id.btnsecond) {
+            value1 = Integer.parseInt(btnfirst.getText().toString());
+            value2 = Integer.parseInt(btnsecond.getText().toString());
+            if (value2 > value1) {
+                points = points + 1;
+            } else {
+                points = points - 1;
+            }
+
+            txtPoints.setText(String.valueOf(points));
 
         }
     }
